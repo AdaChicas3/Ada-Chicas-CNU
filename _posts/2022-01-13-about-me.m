78c9d2e84a98530e21b0f72eee9c21386f6de9b0
AdaChicas3 <97712894+AdaChicas3@users.noreply.github.com>
2022-01-13 21:46:18 -0500

    Update 2022-01-13-about-me.m

--- _posts/2022-01-13-about-me.m
+++ _posts/2022-01-13-about-me.m
@@ -1,3 +1,3 @@
 # Introduction
-## Who Am I? 
-#### Hi! Welcome to my webblog! Little introdution, Im a CNU Engligh major with a minor in Linguistics! I'm a lover of languages and cultures and I am always up to learning new cultural aspects of life! 
+ Who Am I? 
+ Hi! Welcome to my webblog! Little introdution, Im a CNU Engligh major with a minor in Linguistics! I'm a lover of languages and cultures and I am always up to learning new cultural aspects of life! 
